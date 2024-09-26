a0ab6444a9a2fd596c951f2c241b04eafd9065be
SaraShoukry <s.yahia93@gmail.com>
2024-09-26 12:50:03 +0300

    update genric_content design

--- resources/views/generic_contents/show.blade.php
+++ resources/views/generic_contents/show.blade.php
@@ -1,14 +1,16 @@
 @extends('admin.master')
 
 @section('content')
-    <h1>Generic Content Details</h1>
+    <div class="container">
+        <h1>Generic Content Details</h1>
 
-    <div>
-        <strong>Key:</strong> {{ $content->key }}
-    </div>
-    <div>
-        <strong>Value:</strong> {{ $content->value }}
-    </div>
+        <div class="mb-3">
+            <strong>Key:</strong> {{ $content->key }}
+        </div>
+        <div class="mb-3">
+            <strong>Value:</strong> {{ $content->value }}
+        </div>
 
-    <a href="{{ route('generic-content.index') }}">Back to list</a>
+        <a href="{{ route('generic-content.index') }}" class="btn btn-secondary">Back to list</a>
+    </div>
 @endsection
